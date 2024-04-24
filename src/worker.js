efa26ba825b82439bf52faf5720e4dcb79e483d7
Florentin Sakwiset <florentin@wemake.cx>
2024-04-25 00:35:32 +0200

    Add email function to insert email data into the database

--- src/worker.js
+++ src/worker.js
@@ -1,7 +1,17 @@
 export default {
   async email(message, env, ctx) {
-    /**
-     * Create your code
-     */
+    // Connect to D1 database
+    const db = env.DB
+
+    // Extract data from the message
+    const email = message.from
+    const subject = message.subject
+    const body = message.body
+
+    // Prepare an SQL query to insert the data into the database
+    const query = `INSERT INTO emails (email, subject, body) VALUES (?, ?, ?)`
+
+    // Execute the query
+    await db.prepare(query).bind([email, subject, body]).run()
   }
 }
